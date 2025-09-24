ec1182be286d7859e13ac4162c853b9a67cebe08
kevin alejandro fuentes barrales <24470056@campeche.tecnm.mx>
2025-09-24 12:01:48 -0600

    version que utiliza verticalstacklayout como elemento raiz

---
+++ ContadorManual.Gui/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿namespace ContadorManual.Gui
+{
+    public partial class MainPage : ContentPage
+   
+    {
+        //m
+        private int conteo;
+        public MainPage()
+        {
+
+            InitializeComponent();
+            conteo = 0;
+            conteolabel.Text = conteo.ToString();
+        }
+        
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            conteo++;
+            conteolabel.Text = conteo.ToString();
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            conteo = 0;
+            conteolabel.Text = conteo.ToString();
+        }
+
+    }
+}
